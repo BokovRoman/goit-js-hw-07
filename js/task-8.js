8b64b37af74d8ff48f5ceea7b877c0cbbcd953c2
Roman <r.bokov1991@gmail.com>
2021-02-28 16:18:12 +0200

    add task-8

--- js/task-8.js
+++ js/task-8.js
@@ -20,4 +20,50 @@
 
 // <div id="boxes"></div>
 
+const controlsRef = document.querySelector('#controls');
+const inputValueRef = controlsRef.firstElementChild;
+// console.log(inputValueRef);
+const btnCLearRef = document.querySelector('[data-action="destroy"]');
+const btnCreateRef = document.querySelector('[data-action="render"]');
+const boxesRef = document.querySelector('#boxes');
 
+inputValueRef.addEventListener('input', onValueInput);
+
+
+inputValueRef.addEventListener('input', () => {
+    btnCreateRef.addEventListener('click', createBoxes);
+});
+btnCLearRef.addEventListener('click', destroyBoxes);
+    
+    btnCLearRef.addEventListener('click', () => {
+    btnCreateRef.removeEventListener('click', createBoxes);
+});
+
+let amountBoxes = 0;
+function onValueInput(event) {
+    return amountBoxes = Number(event.currentTarget.value);
+    // return amount;
+};
+// console.log(amount);
+
+function createBoxes(amount) {
+    let pixels = 30;
+    let boxes = [];
+    amount = amountBoxes;
+    for (let i = 0; i < amount; i += 1) {
+        const newPixels = pixels + i * 10;
+        const newBox = document.createElement('div');
+        newBox.style.width = `${newPixels}px`;
+        newBox.style.height = `${newPixels}px`;
+        newBox.style.border = '1px solid black';
+        
+        newBox.style.backgroundColor = `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+        boxes.push(newBox);
+    }
+    boxesRef.append(...boxes); 
+};
+
+function destroyBoxes() {
+    boxesRef.innerHTML = '';
+    inputValueRef.value = 0;
+};
